5b268d5189cb7453e7ec1aff27cf3c784f564271
YongHun Lee <yosiyong@gmail.com>
2021-05-30 01:04:57 +0900

    Cardコンポネント Organism作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import "./styles.css";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
+const user = {
+  name: "わんわん",
+  image:
+    "https://images.unsplash.com/photo-1613470131095-8d36d9a3a0c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  email: "yosiyong@gmail.com",
+  phone: "090-3433-8173",
+  company: { name: "GTIAC" },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>テスト</SecondaryButton>
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
